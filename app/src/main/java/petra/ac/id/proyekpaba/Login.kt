323da7808e36c4395abf4893e5256c6d252e9a44
Mark <markus.nugraha14@gmail.com>
2023-12-19 11:32:05 +0700

    Database Account (Login, Sign Up) 2

--- app/src/main/java/petra/ac/id/proyekpaba/Login.kt
+++ app/src/main/java/petra/ac/id/proyekpaba/Login.kt
@@ -1,47 +1,109 @@
 package petra.ac.id.proyekpaba
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import com.google.firebase.Firebase
-import com.google.firebase.firestore.firestore
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Login : AppCompatActivity() {
 
-    private val db = Firebase.firestore
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val inputUsername = findViewById<EditText>(R.id.ed_username)
+        val inputUsernameOrEmail = findViewById<EditText>(R.id.ed_usernameoremail)
         val inputPassword = findViewById<EditText>(R.id.ed_password)
 
         val _btnLogin = findViewById<Button>(R.id.btn_login)
         _btnLogin.setOnClickListener {
 
-            if (inputUsername.text.isNotEmpty() && inputPassword.text.isNotEmpty()) {
+            val usernameOrEmail = inputUsernameOrEmail.text.toString()
+            val password = inputPassword.text.toString()
 
-                val intent= Intent(this@Login, Home::class.java)
-                startActivity(intent)
+            if (usernameOrEmail.isNotEmpty() && password.isNotEmpty()) {
+
+                var isEmailExist: Boolean
+                var isUsernameExist: Boolean
+
+                // Check apakah email sudah pernah dibuat
+                db.collection("tbUserAccount")
+                    .whereEqualTo("emailAkun", usernameOrEmail)
+                    .get()
+                    .addOnSuccessListener { result ->
+                        isEmailExist = !result.isEmpty
+
+                        // Check apakah username sudah pernah dibuat
+                        db.collection("tbUserAccount")
+                            .whereEqualTo("namaAkun", usernameOrEmail)
+                            .get()
+                            .addOnSuccessListener { resultUsername ->
+                                isUsernameExist = !resultUsername.isEmpty
+
+                                if (!isEmailExist && !isUsernameExist) {
+                                    Toast.makeText(
+                                        this,
+                                        "Akun tidak ditemukan",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                } else if(isEmailExist) {
+                                    // Check Password
+                                    val userDoc = result.documents[0]
+                                    val storedPassword = userDoc.getString("passwordAkun")
+
+                                    if (storedPassword == password) {
+                                        val intent = Intent(this@Login, Home::class.java)
+                                        startActivity(intent)
+                                    } else {
+                                        Toast.makeText(
+                                            this,
+                                            "Password salah",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+
+                                } else if(isUsernameExist) {
+                                    // Check Password
+                                    val userDoc = resultUsername.documents[0]
+                                    val storedPassword = userDoc.getString("passwordAkun")
+
+                                    if (storedPassword == password) {
+                                        val intent = Intent(this@Login, Home::class.java)
+                                        startActivity(intent)
+                                    } else {
+                                        Toast.makeText(
+                                            this,
+                                            "Password salah",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                                }
+                            }
+                    }
+            } else {
+                Toast.makeText(
+                    this,
+                    "Data tidak boleh kosong",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
 
         val _txtForgotPass = findViewById<TextView>(R.id.txt_forgotPassword)
         _txtForgotPass.setOnClickListener {
-            // Toast "E-mail has sent for reset password"
+            // Add code for password reset functionality
+            // Toast.makeText(this, "E-mail has sent for reset password", Toast.LENGTH_SHORT).show()
         }
 
         val _txtSignUp = findViewById<TextView>(R.id.txt_signUp)
         _txtSignUp.setOnClickListener {
-            val intent= Intent(this@Login, SignUp::class.java)
+            val intent = Intent(this@Login, SignUp::class.java)
             startActivity(intent)
         }
     }
-
-
 }
--- app/src/main/java/petra/ac/id/proyekpaba/SignUp.kt
+++ app/src/main/java/petra/ac/id/proyekpaba/SignUp.kt
@@ -1,19 +1,19 @@
 package petra.ac.id.proyekpaba
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import com.google.firebase.Firebase
-import com.google.firebase.firestore.firestore
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
 
 class SignUp : AppCompatActivity() {
 
-    private val db = Firebase.firestore
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -25,42 +25,82 @@ class SignUp : AppCompatActivity() {
         val _btnsignUp = findViewById<Button>(R.id.btn_signup)
         _btnsignUp.setOnClickListener {
 
-            if (inputUsername.text.isNotEmpty() && inputPassword.text.isNotEmpty() && inputEmail.text.isNotEmpty()) {
+            val email = inputEmail.text.toString()
+            val username = inputUsername.text.toString()
+            val password = inputPassword.text.toString()
 
-                val dataBaru = Account(
-                    inputEmail.text.toString(),
-                    inputUsername.text.toString(),
-                    inputPassword.text.toString()
-                )
+            if (email.isNotEmpty() && username.isNotEmpty() && password.isNotEmpty()) {
 
+                var isEmailTaken: Boolean
+                var isUsernameTaken: Boolean
+
+                // Check apakah email sudah pernah dibuat
                 db.collection("tbUserAccount")
-                    .document(inputEmail.text.toString())
-                    .set(dataBaru).addOnSuccessListener {
-                        inputUsername.setText("")
-                        inputUsername.setText("")
-                        inputPassword.setText("")
+                    .whereEqualTo("emailAkun", email)
+                    .get()
+                    .addOnSuccessListener { result ->
+                        isEmailTaken = !result.isEmpty
 
-                        Toast.makeText(
-                            this,
-                            "Data berhasil ditambahkan",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                    .addOnFailureListener { e ->
-                        Log.d("PROJ_PABA", "Error adding document", e)
-                    }
+                        // Check apakah username sudah pernah dibuat
+                        db.collection("tbUserAccount")
+                            .whereEqualTo("namaAkun", username)
+                            .get()
+                            .addOnSuccessListener { resultUsername ->
+                                isUsernameTaken = !resultUsername.isEmpty
 
-                val intent= Intent(this@SignUp, Login::class.java)
-                startActivity(intent)
+                                if (!isEmailTaken && !isUsernameTaken) {
+                                    // Tambahkan akun baru
+                                    val dataBaru = Account(username, password, email)
+                                    db.collection("tbUserAccount")
+                                        .document(email)
+                                        .set(dataBaru)
+                                        .addOnSuccessListener {
+                                            inputUsername.setText("")
+                                            inputPassword.setText("")
+                                            inputEmail.setText("")
+
+                                            Toast.makeText(
+                                                this,
+                                                "Akun Berhasil Dibuat",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+
+                                            val intent = Intent(this@SignUp, Login::class.java)
+                                            startActivity(intent)
+                                        }
+                                        .addOnFailureListener { e ->
+                                            Log.d("PROJ_PABA", "Error adding document", e)
+                                        }
+                                } else {
+                                    if (isEmailTaken) {
+                                        Toast.makeText(
+                                            this,
+                                            "E-mail sudah pernah dipakai",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                                    if (isUsernameTaken) {
+                                        Toast.makeText(
+                                            this,
+                                            "Username sudah pernah dipakai",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                                }
+                            }
+                    }
             } else {
-
+                Toast.makeText(
+                    this,
+                    "Data tidak boleh kosong",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
-
         }
 
         val _txtSignUp = findViewById<TextView>(R.id.txt_logIn)
         _txtSignUp.setOnClickListener {
-            val intent= Intent(this@SignUp, Login::class.java)
+            val intent = Intent(this@SignUp, Login::class.java)
             startActivity(intent)
         }
     }
